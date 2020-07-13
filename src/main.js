9a6fd00f9996e56a866c069e2270366b27aaff74
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-07-12 21:45:14 -0300

    Funcionalidades updateCartLenght e renderCart adicionadas!

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ class App {
   constructor() {
     this.cart = [];
 
-    this.divProducts = document.querySelector('.products');
+    this.productsDiv = document.querySelector('.products');
     
-    this.carrinhoMenu = document.querySelector('header .cart a');
-    this.carrinho = document.querySelector('.carrinho');
+    this.cartMenu = document.querySelector('header .cart a');
+    this.cartDiv = document.querySelector('.carrinho');
   }
 
   async addProducts() {
@@ -84,7 +84,7 @@ class App {
         divProduct.appendChild(divImg);
         divProduct.appendChild(divInfo);
 
-        this.divProducts.appendChild(divProduct);
+        this.productsDiv.appendChild(divProduct);
 
         // Adicionando a classe ativo nas primeiras imagens dos menus
         document.querySelectorAll('.product .tab-nav ul').forEach(ul => {
@@ -98,13 +98,8 @@ class App {
 
   openCart(event) {
     event.preventDefault();
-    this.carrinho.classList.toggle('ativo');
+    this.cartDiv.classList.toggle('ativo');
   }
-
-  addFavorite(event) {
-    event.preventDefault();
-    event.currentTarget.firstElementChild.classList.toggle('ativo');
-  } 
 
   changeMainImage(event) {
     event.preventDefault();  
@@ -127,11 +122,125 @@ class App {
     imagem.src = target.src;
   }
 
-  registerHandlers() {
-    this.carrinhoMenu.onclick = event => this.openCart(event);
+  addFavorite(event) {
+    event.preventDefault();
+    event.currentTarget.firstElementChild.classList.toggle('ativo');
+  } 
 
-    this.favoritos.forEach(favorito => favorito.onclick = event => this.addFavorite(event));
+  updateCartLenght() {
+    // Pegando o elemento que contém a quantidade de itens do carrinho
+    const qtde = this.cartMenu.firstElementChild.firstElementChild;
+
+    // Atualizando a quantidade de itens da DOM de acordo com o tamanho da array this.cart
+    qtde.innerText = this.cart.length;
+    
+    if (!+qtde.innerText) {
+      this.cartMenu.firstElementChild.classList.add('hide');
+    } else {
+      this.cartMenu.firstElementChild.classList.remove('hide');
+    }
+    
+  }
+
+  addToCart(event) {
+    const target = event.currentTarget;
+    const targetPai = target.parentElement.parentElement.parentElement;
+    console.log(targetPai)
+
+    const urlImagem = targetPai.querySelectorAll('.tab-nav ul li a img')[0].src;
+    const name = targetPai.querySelector('.info .title h1').innerText;
+    const installments = +(targetPai.querySelector('.info .prices h2 .parcela').innerText);
+    const installmentPrice = +(targetPai.querySelector('.info .prices h2 .parcelado').innerText.replace(',', '.'));
+    const price = +(targetPai.querySelector('.info .prices h3 span').innerText.replace('R$', '').replace('.', '').replace(',', '.'));
+
+    // Conferindo se já há um item no carrinho
+    const arrayNomes = this.cart.map(item => item.urlImagem).includes(urlImagem);
+    if (!arrayNomes) {
+      this.cart.push({
+        urlImagem,
+        name,
+        installments,
+        installmentPrice,
+        price,
+      });
+    }
+
+    console.log(this.cart)
+    this.renderCart();
+  }
+
+  registerHandlers() {
+    this.cartMenu.onclick = event => this.openCart(event);
+
     this.imagesNav.forEach(imageNav => imageNav.onclick = event => this.changeMainImage(event));
+    this.favorites.forEach(favorito => favorito.onclick = event => this.addFavorite(event));
+    this.btnsAddToCart.forEach(btnAddToCart => btnAddToCart.onclick = event => this.addToCart(event));
+  }
+
+  renderCart() {
+    // Pegando a div carrinho-items
+    const cartItem = this.cartDiv.firstElementChild;
+
+    // Zerando o conteúdo da div 
+    cartItem.innerHTML = ''; 
+
+    // Preenchendo a div com os itens presentes no array this.cart
+    this.cart.forEach(cart => {    
+      console.log(cart)
+      let { urlImagem, name, installments, installmentPrice, price } = cart;
+      
+      installmentPrice = cart.installmentPrice.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+      price = cart.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+  
+      const divCarrinhoItem = document.createElement('div');
+      divCarrinhoItem.classList.add('carrinho-item');
+  
+      divCarrinhoItem.innerHTML = `
+        <div class="left">
+          <div class="image-carrinho" style="background-image: url('${urlImagem}');">
+          </div>
+
+          <div class="info-carrinho">
+            <h1 class="name-carrinho">${name}</h1>
+            <h2 class="installments-carrinho">${installments}x ${installmentPrice}</h2>
+            <h2 class="price-carrinho">${price} à vista</h2>
+          </div>
+        </div>
+
+        <a href="#">X</a>
+      `;
+      
+      cartItem.appendChild(divCarrinhoItem);
+    });
+    
+
+    // Armazenando a soma dos preços dos itens
+    const total = this.cart.map(cart => cart.price).reduce((acc, cur) => acc + cur);
+    
+    // Armezenando o menor nº de parcelas presente nos itens
+    const minInstallment = this.cart.map(cart => cart.installments).reduce((acc, cur) => (cur < acc) ? cur : acc);
+    
+    // Convertando os valores para o padrão de moeda R$
+    const totalConvertido = total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    const totalParcelado = (total / minInstallment).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    
+    
+    // Preenchendo o subtotal do carrinho
+    const divSubtotal = document.createElement('div');
+    divSubtotal.classList.add('subtotal');
+
+    divSubtotal.innerHTML = `
+      <div class="subtotal">
+        <h2 class="subtotal-carrinho">subtotal</h2>
+        <h2 class="subtotal-installments">${minInstallment}x de ${totalParcelado}</h2>
+        <h2 class="subtotal-price">ou ${totalConvertido} à vista</h2>
+      </div>
+    `;
+
+    cartItem.appendChild(divSubtotal);
+
+    // Atualizando a quantidade de itens presentes no carrinho
+    this.updateCartLenght();
   }
 
   async init() {
@@ -139,9 +248,10 @@ class App {
     await this.addProducts();
     
     // Guardando os botões para favoritar, só depois de adicionar todos os produtos no DOM 
-    this.favoritos = document.querySelectorAll('.product .info .title h1 a');
     this.imagesNav = document.querySelectorAll('.product .tab-nav ul li a');
-
+    this.favorites = document.querySelectorAll('.product .info .title h1 a');
+    this.btnsAddToCart = document.querySelectorAll('.add-carrinho');
+    this.updateCartLenght();
     this.registerHandlers();
   }
 
